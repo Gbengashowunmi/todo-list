44280d1d66f5db1638afd44c5edcd1edbc538c58
Gbengashowunmi <96231197+Gbengashowunmi@users.noreply.github.com>
2022-12-17 01:16:04 +0100

    fixing package.json

--- src/Alert.js
+++ src/Alert.js
@@ -1,12 +1,12 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 
-const Alert = ({showAlert, alertFunction, items}) => {
-  useEffect(() => {
-  const timeout = setTimeout(() => {
-    alertFunction();
-  }, 4000);
-  return () => clearTimeout(timeout);
-}, [items]);
+const Alert = ({showAlert}) => {
+//   useEffect(() => {
+//   const timeout = setTimeout(() => {
+//     alertFunction();
+//   }, 4000);
+//   return () => clearTimeout(timeout);
+// }, [items]);
   return <p className={`alert alert-${showAlert.type}`}>{showAlert.message}</p>
 }
 
